c59897465e38dc956a341d472c302be65cbb16ee
griffmurphy99 <126801077+griffmurphy99@users.noreply.github.com>
2023-03-02 11:22:32 -0500

    Delete Murphy1.py

--- Murphy1.py
+++
@@ -1,8 +0,0 @@
-from math import pi #imports the number for pi
-r = float(input ("Input the radius of the circle : ")) #ask user to input the radius
-
-print ("The circumference of a circle with a radius of " + str(r) + " is: " + str(2 * pi * r)) #calculates circumference using formula
-
-print ("The area of a circle with a radius of " + str(r) + " is: " + str(pi * r**2)) #calculates area using formula
-
-print ("The volume of a sphere with a radius of " + str(r) + " is: " + str((4/3) * pi * r**3)) #calculates volume using formula
